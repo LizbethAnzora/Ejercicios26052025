bcce9cc15320886f244f9901de00c473404cdc18
Eneida Anzora <anzoraeneida@outlook.com>
2025-05-25 23:12:28 -0600

    Se desarrollo la clase "Main" y se finalizo el ejercicioCincoProfesion

--- EjercicioCincoProfesion/Main.java
+++ EjercicioCincoProfesion/Main.java
@@ -1,5 +1,16 @@
 package EjercicioCincoProfesion;
 
 public class Main {
+    public static void main(String[] args) {
+        Maestro maestro = new Maestro("Ana", 150, 12.50);
+        Arquitecto arquitecto = new Arquitecto("Luis", 180, 18.75);
 
+        System.out.println("Profesión: Maestro");
+        System.out.println("Nombre: " + maestro.getNombre());
+        System.out.println("Sueldo: $" + maestro.calcularSueldo());
+
+        System.out.println("\nProfesión: Arquitecto");
+        System.out.println("Nombre: " + arquitecto.getNombre());
+        System.out.println("Sueldo: $" + arquitecto.calcularSueldo());
+    }
 }
