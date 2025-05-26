be96a2225e3d680af8561fde3518bf8bcb1405af
Eneida Anzora <anzoraeneida@outlook.com>
2025-05-25 21:13:32 -0600

    Se realizo la clase "Vehiculo" para el ejercicioTresVehiculo

--- EjercicioTresVehiculo/Vehiculo.java
+++ EjercicioTresVehiculo/Vehiculo.java
@@ -1,5 +1,39 @@
 package EjercicioTresVehiculo;
 
 public class Vehiculo {
+    private String marca;
+    private String modelo;
+    private String color;
+    
+    
+    public Vehiculo() {
+    }
+    public Vehiculo(String marca, String modelo, String color) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void mover() {
+        System.out.println("El vehículo se está moviendo.");
+}
 
 }
