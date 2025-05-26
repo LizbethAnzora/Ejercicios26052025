113940e471b67330e9e6b32bf2222f3d9ce96df5
Eneida Anzora <anzoraeneida@outlook.com>
2025-05-25 21:34:47 -0600

    Se realizo la clase "Main" finalizando el ejercicioTresVehiculo

--- EjercicioTresVehiculo/Main.java
+++ EjercicioTresVehiculo/Main.java
@@ -1,5 +1,18 @@
 package EjercicioTresVehiculo;
 
 public class Main {
+    public static void main(String[] args) {
+        Vehiculo[] flota = {
+            new Automovil("Toyota", "Hilux", "gris"),
+            new Barco("Lagoon", "52 Fly", "blanco"),
+            new Avion("Korean Air", "A350-941", "celeste")
+        };
+
+        // Polimorfismo: todos llamamos mover(), pero cada clase lo implementa a su manera
+        for (Vehiculo v : flota) {
+            System.out.print(v.getClass().getSimpleName() + ": ");
+            v.mover();
+        }
+    }
 
 }
