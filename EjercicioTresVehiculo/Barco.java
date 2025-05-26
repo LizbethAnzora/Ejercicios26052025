b985e79b7e8ba62a130da2e5907b62219e8b7590
Eneida Anzora <anzoraeneida@outlook.com>
2025-05-25 21:22:08 -0600

    Se realizo la clase "Barco" para el ejercicioTresVehiculo

--- EjercicioTresVehiculo/Barco.java
+++ EjercicioTresVehiculo/Barco.java
@@ -1,5 +1,13 @@
 package EjercicioTresVehiculo;
 
-public class Barco {
+public class Barco extends Vehiculo {
+    public Barco(String marca, String modelo, String color) {
+        super(marca, modelo, color);
+    }
 
+    @Override
+    public void mover() {
+        System.out.println("El barco navega sobre el agua.");
+
+    }
 }
