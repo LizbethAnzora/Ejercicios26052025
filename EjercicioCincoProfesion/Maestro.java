6b7b2ac5283711787cbd4e328d893e7731a378db
Eneida Anzora <anzoraeneida@outlook.com>
2025-05-25 22:50:33 -0600

    Se desarrollo la clase "Maestro" para el ejercicioCincoProfesion

--- EjercicioCincoProfesion/Maestro.java
+++ EjercicioCincoProfesion/Maestro.java
@@ -1,5 +1,46 @@
 package EjercicioCincoProfesion;
 
-public class Maestro {
+public class Maestro implements Profesion {
+    private String nombre;
+    private int clasesImpartidas;
+    private double pagoPorClase;
+    
+    public Maestro() {
+    }
+    
+    public Maestro(String nombre, int clasesImpartidas, double pagoPorClase) {
+        this.nombre = nombre;
+        this.clasesImpartidas = clasesImpartidas;
+        this.pagoPorClase = pagoPorClase;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getClasesImpartidas() {
+        return clasesImpartidas;
+    }
+
+    public void setClasesImpartidas(int clasesImpartidas) {
+        this.clasesImpartidas = clasesImpartidas;
+    }
+
+    public double getPagoPorClase() {
+        return pagoPorClase;
+    }
+
+    public void setPagoPorClase(double pagoPorClase) {
+        this.pagoPorClase = pagoPorClase;
+    }
+
+    @Override
+    public double calcularSueldo() {
+        return clasesImpartidas * pagoPorClase;
+    }
 
 }
